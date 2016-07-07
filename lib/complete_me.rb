167a1e4fa37f0baeeca99715132be152d3b36bfe
PlanetEfficacy <jspevack@gmail.com>
2016-07-07 09:00:17 -0600

    It can suggest word from prefix

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -11,18 +11,38 @@ class CompleteMe
   end
 
   def insert(word)
-
     node = @root
-    something = word.split('').map do |char|
+    word.each_char.map do |char|
       if !node.children.has_key?(char)
         node.children[char] = Node.new
       end
       node = node.children[char]
     end
     node.flag = true
-    #binding.pry
     @count += 1
-    something
+  end
+
+  def suggest(prefix)
+    suggestions = []
+    node = @root
+    prefix.each_char do |char|
+      if !node.children.has_key?(char)
+        return nil
+      end
+      node = node.children[char]
+    end
+    find_all_the_words(node, prefix, suggestions)
+    return suggestions
+  end
+
+  def find_all_the_words(node, prefix, suggestions)
+    suggestions << prefix if node.flag
+    return suggestions if node.children.empty?
+    node.children.keys.each do |char|
+        prefix += char
+        node = node.children[char]
+        find_all_the_words(node, prefix, suggestions)
+    end
   end
 
 end
--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -24,51 +24,107 @@ class CompleteMeTest < Minitest::Test
     assert_equal "Node", class_name
   end
 
-  def test_a_CompleteMe_can_insert_a_word
-    c = CompleteMe.new
-    assert c.insert("Pizza")
-  end
-
   def test_a_CompleteMe_can_update_word_count
     c = CompleteMe.new
     c.insert("pizza")
     assert_equal 1, c.count
+    c.insert("pizzeria")
+    assert_equal 2, c.count
   end
 
   def test_can_see_root_node
     c = CompleteMe.new
     c.insert("pizza")
     assert_instance_of Node, c.root
-
   end
 
-  def test_insert_can_split_word
+  def test_it_can_insert_word
     c = CompleteMe.new
-    assert_instance_of Array, c.insert("pizza")
-  end
-
-  def test_can_iterate_through_each_letter
-    c = CompleteMe.new
-    string = "pizza"
-    length = string.length
-    assert_equal length, c.insert("pizza").length
-  end
-
-  def test_flag_true_on_final_node_after_insert_word
-    c = CompleteMe.new
-    string = "pizza"
-    pizza_array = string.split
-    c.insert(string)
+    c.insert("pizza")
+    expected_node_keys = ["p", "i", "z", "z", "a"]
+    node_keys = []
     node = c.root
-    check_flag = false
-    pizza_array.each do |letter|
-      node = node.children[letter]
-      check_flag = node.flag
+    while node.children.keys.length > 0
+      node_keys << node.children.keys[0]
+      node = node.children[node.children.keys[0]]
     end
-    assert_equal check_flag, true
+    assert_equal node_keys, expected_node_keys
   end
 
+  def test_it_knows_pizza_is_in_the_dictionary
+    skip
+    c = CompleteMe.new
+    c.insert("pizza")
+    assert_equal ["pizza"], c.suggest("pizza")
+  end
 
+  def test_it_can_suggest_word_from_prefix
+    c = CompleteMe.new
+    c.insert("pizza")
+    assert_equal ["pizza"], c.suggest("piz")
+  end
 
+  def test_it_can_insert_words
+    skip
+    c = CompleteMe.new
+    c.insert("pizza")
+    c.insert("pizzeria")
+    expected_node_keys = ["p", "i", "z", "z", "a", "e", "r", "i", "a"]
+    node_keys = []
+    node = c.root
+    until node.children.keys.length == 0
+      node_keys << node.children.keys[0]
+      node = node.children[node.children.keys[0]]
+    end
+    assert_equal expected_node_keys, node_keys
+  end
+
+  def test_it_can_find_words
+  end
 
 end
+
+#STUPID SHIT BELOW
+  # def test_it_can_insert_two_totally_different_words
+  #   c = CompleteMe.new
+  #   c.insert("pi")
+  #   c.insert("at")
+  #   expected_node_keys = ["p", "i", "a", "t"]
+  #   node_keys = []
+  #   node = c.root
+  #   node.each do |child|
+  #     binding.pry
+  #   end
+  #   # while node.children.length > 0
+  #   #   children = node.children
+  #   #   children.each do |child|
+  #   #     if child
+  #   #     end
+  #   #   end
+  #   # end
+  #   #   node.children.each_value do |value|
+  #
+  #
+  # end
+
+  # def test_it_can_insert_two_words
+  #   skip
+  #   c = CompleteMe.new
+  #   c.insert("to")
+  #   c.insert("too")
+  #   expected_node_keys = ["t", "o", "o"]
+  #   node_keys = []
+  #   expected_word_count = 2
+  #   node = c.root
+  #   expected_flags = [false, true, true]
+  #   actual_flags = {}
+  #   binding.pry
+  #   while node.children.keys.length >= 0
+  #     node_keys << node.children.keys[0]
+  #     actual_flags[node.object_id] = node.flag
+  #     node = node.children[node.children.keys[0]]
+  #   end
+  #   assert_equal node_keys, expected_node_keys
+  #   assert_equal c.count, expected_word_count
+  #   assert_equal actual_flags.values, expected_flags
+  # end
